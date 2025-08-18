c2cc9eb1cb8a3e2fe58a3ae0e166c3ddc453339a
Virendra <vdrajput138@gmail.com>
2025-08-18 12:04:08 +0530

    Day 001 Print statement and Input statements

---
+++ Day001/inputStatements.py
@@ -0,0 +1,6 @@
+name = input("Hello!, May i know your name? ")
+print("Hey, " + name + "! \n")
+
+
+
+print("Hello," + input(" What is your name? \t"))
---
+++ Day001/printStatement.py
@@ -0,0 +1,19 @@
+# indentationError
+#   print("Hello " + "" +"World!")
+
+print ("Hello " + "" + "Virendra") 
+print("Hello" + " " + "Virendra")
+
+#SyntaxError
+# print("Hello " + "" + "Virendra) 
+
+
+# Fix the code below 👇
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
+
+
