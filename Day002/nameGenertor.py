5875ba30d8a1aff5c1cd1a5d734400e50ec6f740
Virendra <vdrajput138@gmail.com>
2025-08-19 11:54:23 +0530

    Today's small project on Band Name generator

---
+++ Day002/nameGenertor.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+place = input("Where are you from?")
+petName = input("What is your pet's name?")
+
+randomBandName = name + "'s "+ place + petName + "!"
+print("The random name generated for your band is : " + randomBandName)
