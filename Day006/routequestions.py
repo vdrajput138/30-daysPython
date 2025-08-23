858ade73c32b3c0364a1514e322813aa1bbc0f67
Virendra <vdrajput138@gmail.com>
2025-08-23 23:08:12 +0530

    initial draft

---
+++ Day006/routequestions.py
@@ -0,0 +1,4 @@
+print("You need to travel from Pune to Delhi." \
+"\n You will be provided with some options, on your selection, we will get how much time it will take to reach Delhi." \
+"\n Let's begin!")
+
