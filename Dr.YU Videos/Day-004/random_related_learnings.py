f493041212b7d796a1a89d50d6f109040c1323f6
Virendra <vdrajput138@gmail.com>
2025-08-31 12:05:37 +0530

    Resumed day 4.

---
+++ Dr.YU Videos/Day-004/random_related_learnings.py
@@ -0,0 +1,25 @@
+#Mersenne Twister
+
+import random
+# import my_module
+
+ranNumber = random.randint(1,10)
+# print(ranNumber)
+
+# print(my_module.my_fav_number)
+
+
+# random_between_0_1 = random.random()
+# print(random_between_0_1)
+
+
+
+# random_float = random.uniform(3.12, 4.45)
+
+# print(random_float)
+
+headOrTails = random.randint(0,1)
+if headOrTails == 0:
+    print("Its Heads - "+ str(headOrTails))
+else :
+    print("Its Tails - " + str(headOrTails))
