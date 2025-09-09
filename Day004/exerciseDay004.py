3ae38487d4e1bc6f72f81b403a76031be450dbcd
Virendra <vdrajput138@gmail.com>
2025-09-09 23:51:35 +0530

    Added a small code to select a random movie from random category

--- Day004/exerciseDay004.py
+++ Day004/exerciseDay004.py
@@ -32,6 +32,9 @@ else:
     4. Create a "Random Compliment Generator" that picks from a list of 10 compliments and personalizes it with the user's name.
 """
 
+
+
+
 """
     5. Write a program that generates a random password using a list of characters (letters, numbers, symbols) - make it 6 characters long.
 """
@@ -40,7 +43,25 @@ else:
 """
     6. Build a "What to Watch Tonight" program that randomly selects from different categories (Action, Comedy, Horror) and then picks a random movie from that category.
 """
+cat = ["Action", "Horror", "Comedy","Thriller"]
+amovies = ["A","B", "C"]
+hmovies = ["D","E","F"]
+cmovies = ["G","H","I"]
+tmovies = ["J","K","L"]
 
+catrandom = random.randint(1,4)
+moviRandom = random.randint(1,3)
+movieSelection = [];
+if catrandom == 1:
+    movieSelection = amovies
+elif catrandom ==2:
+    movieSelection = hmovies
+elif catrandom == 3:
+    movieSelection = cmovies
+else:
+    movieSelection = tmovies
+
+print(f"Today, we wil watch {movieSelection[moviRandom-1]} from {cat[catrandom-1]} category")
 
 """
     7. Create a simple lottery number generator that picks 6 unique numbers between 1-49.
