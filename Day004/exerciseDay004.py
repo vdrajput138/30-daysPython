1453ea56ae8a06857fdf3f599a99a9e5c2578dab
Virendra <vdrajput138@gmail.com>
2025-10-13 23:47:56 +0530

    Added a code, but need some more idea on Tuple and Directory

--- Day004/exerciseDay004.py
+++ Day004/exerciseDay004.py
@@ -408,9 +408,17 @@ Categories: produce, dairy, meat, pantry, frozen
 Randomly generate 12 items across categories
 Sort them by optimal shopping path (produce→dairy→meat→pantry→frozen)
 """
+produce = [1,2,3,4,5,6,7,8,9,10]
+meat = [11,12,13,14,15,16,17,18,19,20]
+dairy = [21,22,23,24,25,26,27,28,29,30] 
+pantry = [31,32,33,34,35,36,37,38,39,40]
+frozen = [41,42,43,44,45,46,47,48,49,50]
 
-
-
+combined_products = produce + meat + dairy + pantry + frozen
+print(combined_products)
+selected_random = random.sample(combined_products,12)
+print(selected_random)
+#TODO - Complete this task after learning tuples and directory 
 
 """
 18. Random Language Learning Flashcards
