7a7c77ab0fbe45052a7d5fc108c0204b83cd985b
Virendra <vdrajput138@gmail.com>
2025-10-14 09:37:22 +0530

    Added a new katana

--- Python-codewars/practice.py
+++ Python-codewars/practice.py
@@ -10,3 +10,22 @@ def multiply(a, b):
 print(multiply(1,3))
 print(multiply(2,3))
 print(multiply(10,9))
+
+#============================================================
+
+"""
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+"""
+
+def even_or_odd(number):
+#    if number >= 0:
+        if number %2 == 0:
+            print("The number is Even number")
+            return "Even"
+        else:
+            print("The number is Odd number")
+            return "Odd"
+#    else:
+#        print("Please enter the positive number")
+
+print(even_or_odd(-1))
