dde668bfc4db78f6ab1669a217357ef83e400164
Virendra <vdrajput138@gmail.com>
2025-10-14 09:29:01 +0530

    started alongside codewars

---
+++ Python-codewars/practice.py
@@ -0,0 +1,12 @@
+#This code does not execute properly. Try to figure out why.
+"""def multiply(a, b):
+    a * b
+"""
+
+def multiply(a, b):
+    return a * b
+
+
+print(multiply(1,3))
+print(multiply(2,3))
+print(multiply(10,9))
