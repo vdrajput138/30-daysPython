a3a23b6b1a86b4d83f1e4a0f886899e0ce250171
Virendra <vdrajput138@gmail.com>
2025-10-15 13:51:01 +0530

    added another codewars code

--- Python-codewars/practice.py
+++ Python-codewars/practice.py
@@ -29,3 +29,35 @@ def even_or_odd(number):
 #        print("Please enter the positive number")
 
 print(even_or_odd(-1))
+
+
+#============================================================
+
+"""Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+The correct answer would be 17.
+
+"""
+def count_sheeps(sheep):
+    sheeps_count = 0
+    for nums in sheep:
+         if nums:
+              sheeps_count += 1
+    print(sheeps_count)
+
+
+sheeps = [True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+count_sheeps(sheeps)
